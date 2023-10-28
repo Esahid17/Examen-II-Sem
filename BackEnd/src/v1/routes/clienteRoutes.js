955d3923e8194fb6bc443d0da8d451da3172405a
Jesus Maximiliano Gonzalez Frias <gonzalezfriasmax36@gmail.com>
2023-10-27 18:39:23 -0600

    fix: Mejora del formato de las rutas

--- BackEnd/src/v1/routes/clienteRoutes.js
+++ BackEnd/src/v1/routes/clienteRoutes.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const clienteController = require('../../controllers/clienteController');
 
 router
+    .post('/postCliente', clienteController.createNewCliente)
+
     .get('/getAllClientes', clienteController.getAllClientes)
 
     .get('/getClienteById/:id_cliente', clienteController.getOneCliente)
 
-    .post('/postCliente', clienteController.createNewCliente)
+    .get('/getAllPrestamosByIdCliente/:id_cliente', clienteController.getAllPrestamosByIdCliente)
 
     .put('/updateClienteById/:id_cliente', clienteController.updateOneCliente)
 
-    .delete('/deleteClienteById/:id_cliente', clienteController.deleteOneCliente)
-
-    .get('/getAllPrestamosByIdCliente/:id_cliente', clienteController.getAllPrestamosByIdCliente);
+    .delete('/deleteClienteById/:id_cliente', clienteController.deleteOneCliente);
 
 module.exports = router;
 
