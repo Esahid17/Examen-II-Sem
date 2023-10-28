b6232999f70b9798cb2c4fa460acabbcd799da11
Jesus Maximiliano Gonzalez Frias <gonzalezfriasmax36@gmail.com>
2023-10-27 18:44:07 -0600

    feat: Modelos de la base de datos agregados

---
+++ FrontEnd/src/app/models/Cliente.ts
@@ -0,0 +1,9 @@
+export class Cliente {
+    idCliente: number = 0;
+    nombre: string = '';
+    rfc: string = '';
+    edad: number = 0;
+    fecha_alta: Date = new Date();
+    telefono: string = '';
+    correo: string = '';
+}
---
+++ FrontEnd/src/app/models/Prestamo.ts
@@ -0,0 +1,7 @@
+export class Prestamo {
+    idPrestamo: number = 0;
+    idCliente: number = 0;
+    monto: number = 0;
+    plazo_meses: number = 0;
+    interes: number = 0;
+}
